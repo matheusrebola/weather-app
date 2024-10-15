91e607b6c94b6529267460ab85ad18baa66f834f
Matheus Rebola <58261178+matheusrebola@users.noreply.github.com>
2024-10-15 00:50:12 +0000

    feat: configuracao dos metodos default

--- weather-spring/src/main/java/weatherapp/services/LongTermForecastsService.java
+++ weather-spring/src/main/java/weatherapp/services/LongTermForecastsService.java
@@ -1,5 +1,45 @@
 package weatherapp.services;
 
-public class LongTermForecastsService {
+import java.util.List;
+import java.util.UUID;
 
+import weatherapp.entities.LongTermForecasts;
+import weatherapp.repositories.LongTermForecastsRepository;
+
+@Service 
+@RequiredArgsConstructor
+public class LongTermForecastsService {
+    private final LongTermForecastsRepository longTermForecastsRepository;
+
+    public List<LongTermForecasts> findAll(){
+        return longTermForecastsRepository.findAll();
+    }
+
+    public LongTermForecasts findById(UUID id){
+        return longTermForecastsRepository.fidnById();
+    }
+
+    public LongTermForecasts saveLongTermForecasts(LongTermForecasts longTermForecasts){
+        return longTermForecastsRepository.save(longTermForecasts);
+    }
+
+    public List<Locations> findByCity(String city){
+        return longTermForecastsRepository.findByCity(city);
+    }
+
+    public Locations findByRegion(String region){
+        return longTermForecastsRepository.findByRegion(region);
+    }
+
+    public Locations findByCountry(String country){
+        return longTermForecastsRepository.findByCountry(country);
+    }
+
+    public Locations findByLatitude(String latitude){
+        return longTermForecastsRepository.findByLatitude(latitude);
+    }
+
+    public Locations findByLongitude(String longitude){
+        return longTermForecastsRepository.findByLongitude(longitude);
+    }
 }
