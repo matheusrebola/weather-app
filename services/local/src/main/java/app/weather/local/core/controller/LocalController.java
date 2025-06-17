bd1d7c32ba2a725a78039d32dc1f3a1b6dde929d
Matheus Rebola <m.cavalcantir@outlook.com>
2025-06-17 10:27:31 -0300

    feat: criação dos métodos do endpoint

--- services/local/src/main/java/app/weather/local/core/controller/LocalController.java
+++ services/local/src/main/java/app/weather/local/core/controller/LocalController.java
@@ -1,4 +1,40 @@
 package app.weather.local.core.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/local")
+@RequiredArgsConstructor
 public class LocalController {
+
+    @PostMapping
+    public void receberUsuario(){
+
+    }
+
+    @PutMapping("/{id}")
+    public void atualizarUsuario(@PathVariable String id){
+
+    }
+
+    @GetMapping
+    public void buscarTodos(){
+
+    }
+
+    @GetMapping("/{id}")
+    public void buscarPeloId(@PathVariable String id){
+
+    }
+
+    @DeleteMapping
+    public void deletarTodos(){
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarPeloId(@PathVariable String id){
+
+    }
 }
--- services/notification/src/main/java/app/weather/notification/core/controller/NotificacaoController.java
+++ services/notification/src/main/java/app/weather/notification/core/controller/NotificacaoController.java
@@ -1,4 +1,36 @@
 package app.weather.notification.core.controller;
 
+import org.springframework.web.bind.annotation.*;
+
 public class NotificacaoController {
+
+    @PostMapping
+    public void criarNotificacao(){
+
+    }
+
+    @PutMapping("/{id}")
+    public void atualizarNotificacao(@PathVariable String id){
+
+    }
+
+    @GetMapping
+    public void buscarTodos(){
+
+    }
+
+    @GetMapping("/{id}")
+    public void buscarPeloId(@PathVariable String id){
+
+    }
+
+    @DeleteMapping
+    public void deletarTodos(){
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarPeloId(@PathVariable String id){
+
+    }
 }
